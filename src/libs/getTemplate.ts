a2c1adadba2845424b3a283960dc81e374cc3707
Neo <neo@creatify.ai>
2025-05-28 18:52:11 +0800

    chore: remove yarn.lock

--- src/libs/getTemplate.ts
+++ src/libs/getTemplate.ts
@@ -1,6 +1,8 @@
-import fs from 'fs';
-import path from 'path';
+import fs from "fs";
+import path from "path";
 
 export const getTemplate = (fileName: string) => {
-  return fs.readFileSync(path.join(__dirname, `../templates/${fileName}.template`)).toString();
+	return fs
+		.readFileSync(path.join(__dirname, `../templates/${fileName}.template`))
+		.toString();
 };
